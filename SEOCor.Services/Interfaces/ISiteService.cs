fe7c7875a866fa0eba2c01aa2d18f59d23363a83
tbehunin <tbehunin@gmail.com>
2014-02-17 17:12:48 -0800

    Initial commit of existing code

---
+++ SEOCor.Services/Interfaces/ISiteService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SEOCor.Services.DTO;
+
+namespace SEOCor.Services.Interfaces
+{
+    public interface ISiteService
+    {
+        IList<SiteDTO> GetSites(string userId);
+    }
+}
